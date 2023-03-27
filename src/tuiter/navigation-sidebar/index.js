0462affb3708d554dcb2f984883c17e15d67113c
Ellie Gui <guizx77@gmail.com>
2023-03-26 17:42:16 -0700

    dl

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -20,28 +20,22 @@ const NavigationSidebar = () => {
                 Labs
             </Link>
 
-            <a href="/#" className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
+            <a href="/#" className={`list-group-item ${active === 'notifications'?'active':''}`}>
                 Notifications
             </a>
-            <a href="/#" className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
+            <a href="/#" className={`list-group-item ${active === 'messages'?'active':''}`}>
                 Messages
             </a>
-            <a href="/#" className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
+            <a href="/#" className={`list-group-item ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
             </a>
-            <a href="/#" className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
+            <a href="/#" className={`list-group-item ${active === 'lists'?'active':''}`}>
                 Lists
             </a>
-            <a href="/tuiter/profile" className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
+            <a href="/tuiter/profile" className={`list-group-item ${active === 'profile'?'active':''}`}>
                 Profile
             </a>
-            <a href="/#" className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
+            <a href="/#" className={`list-group-item ${active === 'more'?'active':''}`}>
                 More
             </a>
         </div>
