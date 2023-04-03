589c207944aba7ff66584d2e3703a89f16d40c21
Ellie Gui <guizx77@gmail.com>
2023-04-02 17:20:11 -0700

    a8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const TUITS_API = 'https://tuiter-node-server-app-zo3p.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${API_BASE}/api/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
